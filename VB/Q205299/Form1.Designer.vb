bb0891da11ec967a21c144472e993f432e05908d
DevExpressExampleBot <fakeEmail>
2019-02-15 23:04:14 +0300

    Source auto update [skip ci]

--- VB/Q205299/Form1.Designer.vb
+++ VB/Q205299/Form1.Designer.vb
@@ -1,101 +1,99 @@
-Imports Microsoft.VisualBasic
-Imports System
-Namespace Q205299
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+﻿Namespace Q205299
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#Region "Windows Form Designer generated code"
+        #Region "Windows Form Designer generated code"
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.components = New System.ComponentModel.Container()
-			Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
-			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
-			Me.gridControl2 = New DevExpress.XtraGrid.GridControl()
-			Me.gridView2 = New DevExpress.XtraGrid.Views.Grid.GridView()
-			Me.bindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
-			Me.bindingSource2 = New System.Windows.Forms.BindingSource(Me.components)
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.gridControl2, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.gridView2, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.bindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.bindingSource2, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' gridControl1
-			' 
-			Me.gridControl1.Location = New System.Drawing.Point(12, 12)
-			Me.gridControl1.MainView = Me.gridView1
-			Me.gridControl1.Name = "gridControl1"
-			Me.gridControl1.ShowOnlyPredefinedDetails = True
-			Me.gridControl1.Size = New System.Drawing.Size(400, 393)
-			Me.gridControl1.TabIndex = 0
-			Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
-			' 
-			' gridView1
-			' 
-			Me.gridView1.GridControl = Me.gridControl1
-			Me.gridView1.Name = "gridView1"
-			' 
-			' gridControl2
-			' 
-			Me.gridControl2.Location = New System.Drawing.Point(428, 12)
-			Me.gridControl2.MainView = Me.gridView2
-			Me.gridControl2.Name = "gridControl2"
-			Me.gridControl2.Size = New System.Drawing.Size(400, 393)
-			Me.gridControl2.TabIndex = 1
-			Me.gridControl2.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView2})
-			' 
-			' gridView2
-			' 
-			Me.gridView2.GridControl = Me.gridControl2
-			Me.gridView2.Name = "gridView2"
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(844, 411)
-			Me.Controls.Add(Me.gridControl2)
-			Me.Controls.Add(Me.gridControl1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.gridControl2, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.gridView2, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.bindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.bindingSource2, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+            Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+            Me.gridControl2 = New DevExpress.XtraGrid.GridControl()
+            Me.gridView2 = New DevExpress.XtraGrid.Views.Grid.GridView()
+            Me.bindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+            Me.bindingSource2 = New System.Windows.Forms.BindingSource(Me.components)
+            CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.gridControl2, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.gridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.bindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.bindingSource2, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' gridControl1
+            ' 
+            Me.gridControl1.Location = New System.Drawing.Point(12, 12)
+            Me.gridControl1.MainView = Me.gridView1
+            Me.gridControl1.Name = "gridControl1"
+            Me.gridControl1.ShowOnlyPredefinedDetails = True
+            Me.gridControl1.Size = New System.Drawing.Size(400, 393)
+            Me.gridControl1.TabIndex = 0
+            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
+            ' 
+            ' gridView1
+            ' 
+            Me.gridView1.GridControl = Me.gridControl1
+            Me.gridView1.Name = "gridView1"
+            ' 
+            ' gridControl2
+            ' 
+            Me.gridControl2.Location = New System.Drawing.Point(428, 12)
+            Me.gridControl2.MainView = Me.gridView2
+            Me.gridControl2.Name = "gridControl2"
+            Me.gridControl2.Size = New System.Drawing.Size(400, 393)
+            Me.gridControl2.TabIndex = 1
+            Me.gridControl2.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView2})
+            ' 
+            ' gridView2
+            ' 
+            Me.gridView2.GridControl = Me.gridControl2
+            Me.gridView2.Name = "gridView2"
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(844, 411)
+            Me.Controls.Add(Me.gridControl2)
+            Me.Controls.Add(Me.gridControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.gridControl2, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.gridView2, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.bindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.bindingSource2, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
 
-		End Sub
+        End Sub
 
-		#End Region
+        #End Region
 
-		Private gridControl1 As DevExpress.XtraGrid.GridControl
-		Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
-		Private gridControl2 As DevExpress.XtraGrid.GridControl
-		Private gridView2 As DevExpress.XtraGrid.Views.Grid.GridView
-		Private bindingSource1 As System.Windows.Forms.BindingSource
-		Private bindingSource2 As System.Windows.Forms.BindingSource
-	End Class
+        Private gridControl1 As DevExpress.XtraGrid.GridControl
+        Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+        Private gridControl2 As DevExpress.XtraGrid.GridControl
+        Private gridView2 As DevExpress.XtraGrid.Views.Grid.GridView
+        Private bindingSource1 As System.Windows.Forms.BindingSource
+        Private bindingSource2 As System.Windows.Forms.BindingSource
+    End Class
 End Namespace
 
--- VB/Q205299/Form1.vb
+++ VB/Q205299/Form1.vb
@@ -1,60 +1,56 @@
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.ComponentModel
-Imports System.Data
-Imports System.Drawing
-Imports System.Text
+﻿Imports System.Data
 Imports System.Windows.Forms
 
 Namespace Q205299
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-			Dim dataSet11 As New DataSet()
-			dataSet11.Tables.Add(GetCustomerDataTable())
-			dataSet11.Tables.Add(GetPersonDataTable())
-			Dim keyColumn As DataColumn = dataSet11.Tables("Customers").Columns("ID")
-			Dim foreignKeyColumn As DataColumn = dataSet11.Tables("Persons").Columns("ID")
-			dataSet11.Relations.Add("CustomersPersons", keyColumn, foreignKeyColumn)
+    Partial Public Class Form1
+        Inherits Form
 
-			bindingSource1.DataSource = dataSet11
-			bindingSource1.DataMember = "Customers"
-			bindingSource2.DataSource = bindingSource1
-			bindingSource2.DataMember = "CustomersPersons"
+        Public Sub New()
+            InitializeComponent()
+            Dim dataSet11 As New DataSet()
+            dataSet11.Tables.Add(GetCustomerDataTable())
+            dataSet11.Tables.Add(GetPersonDataTable())
+            Dim keyColumn As DataColumn = dataSet11.Tables("Customers").Columns("ID")
+            Dim foreignKeyColumn As DataColumn = dataSet11.Tables("Persons").Columns("ID")
+            dataSet11.Relations.Add("CustomersPersons", keyColumn, foreignKeyColumn)
 
-			gridControl1.DataSource = bindingSource1
-			gridControl2.DataSource = bindingSource2
-		End Sub
-		Private Function GetCustomerDataTable() As DataTable
-			Dim table As New DataTable()
-			table.TableName = "Customers"
-			table.Columns.Add(New DataColumn("Items", GetType(String)))
-			table.Columns.Add(New DataColumn("Money", GetType(Double)))
-			table.Columns.Add(New DataColumn("ID", GetType(Integer)))
-			For i As Integer = 0 To 9
-				table.Rows.Add("Product " & i, 3000 + i * 298.55D, i)
-			Next i
-			Return table
-		End Function
-		Private Function GetPersonDataTable() As DataTable
-			Dim table As New DataTable()
-			table.TableName = "Persons"
-			table.Columns.Add(New DataColumn("FirstName", GetType(String)))
-			table.Columns.Add(New DataColumn("SecondName", GetType(String)))
-			table.Columns.Add(New DataColumn("Age", GetType(Integer)))
-			table.Columns.Add(New DataColumn("ID", GetType(Integer)))
-			For i As Integer = 0 To 49
-				Dim name As String = "Adam"
-				Dim secondName As String = "Smith"
-				If i Mod 2 = 0 Then
-					name = "Ben"
-					secondName = "Black"
-				End If
-				table.Rows.Add(name, secondName, 20 + i \ 2, i Mod 10)
-			Next i
-			Return table
-		End Function
-	End Class
+            bindingSource1.DataSource = dataSet11
+            bindingSource1.DataMember = "Customers"
+            bindingSource2.DataSource = bindingSource1
+            bindingSource2.DataMember = "CustomersPersons"
+
+            gridControl1.DataSource = bindingSource1
+            gridControl2.DataSource = bindingSource2
+        End Sub
+        Private Function GetCustomerDataTable() As DataTable
+            Dim table As New DataTable()
+            table.TableName = "Customers"
+            table.Columns.Add(New DataColumn("Items", GetType(String)))
+            table.Columns.Add(New DataColumn("Money", GetType(Double)))
+            table.Columns.Add(New DataColumn("ID", GetType(Integer)))
+            For i As Integer = 0 To 9
+                table.Rows.Add("Product " & i, 3000 + i * 298.55D, i)
+            Next i
+            Return table
+        End Function
+        Private Function GetPersonDataTable() As DataTable
+            Dim table As New DataTable()
+            table.TableName = "Persons"
+            table.Columns.Add(New DataColumn("FirstName", GetType(String)))
+            table.Columns.Add(New DataColumn("SecondName", GetType(String)))
+            table.Columns.Add(New DataColumn("Age", GetType(Integer)))
+            table.Columns.Add(New DataColumn("ID", GetType(Integer)))
+            For i As Integer = 0 To 49
+
+                Dim name_Renamed As String = "Adam"
+                Dim secondName As String = "Smith"
+                If i Mod 2 = 0 Then
+                    name_Renamed = "Ben"
+                    secondName = "Black"
+                End If
+                table.Rows.Add(name_Renamed, secondName, 20 + i \ 2, i Mod 10)
+            Next i
+            Return table
+        End Function
+    End Class
 End Namespace
